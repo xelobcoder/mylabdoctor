92a5cdebf321bf891ef680cd7d9101a6b6ce54d8
Xelob coder <xelobcoder@gmail.com>
2023-12-09 08:07:46 -0800

    watinglist added

--- app/request/page.js
+++ app/request/page.js
@@ -2,10 +2,12 @@
 import Divider from "@/components/Divider";
 import { useState } from "react";
 import { handleSearch } from "./billing/util";
+import { useRouter } from "next/navigation";
 
 export default function RequestTest() {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const router = useRouter();
   return (
     <Divider>
       <div style={{width:'80%'}}>
@@ -18,7 +20,7 @@ export default function RequestTest() {
         </button>
       </div>
       
-      <div style={{marginTop:'20px !important'}}>
+      <div style={{marginTop:'30px'}}>
       {results.length > 0 ? (
         <div className="list-group">
           {
@@ -30,13 +32,19 @@ export default function RequestTest() {
                   <p>DOB: {new Date(item?.dob).toLocaleDateString() || ""}</p>
                   <p>Marital Status: {item?.marital_status}</p>
                   <p>Mobile Number: {item?.mobile_number}</p>
+                  <p>
+                    <button onClick={(ev) =>  {router.push(`/request/billing?patientid=${item?.patientid}`)}} className="btn-success p-1 mx-1" style={{marginTop:'-10px'}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                    </svg>{ " "}request test
+                    </button>
+                  </p>
                 </div>
               )
             })
           }
         </div>
       ) : (
-        <div className="list-group">
+        <div className="list-group mt-1">
           <div className="list-group-item d-flex justify-content-between">
             <p>No Data Available</p>
           </div>
--- app/waitinglist/page.js
+++ app/waitinglist/page.js
@@ -1,9 +1,61 @@
+"use client";
 import Divider from "@/components/Divider";
+import { customFetch } from "@/components/request/util";
+import { useCallback, useEffect, useState } from "react";
 
 export default function WaitingList() {
+  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState({ type: '', startdate: '', enddate: '' });
+  const [data, setData] = useState([]);
+
+  const getData = useCallback(async (clinicianid) => {
+    const response = await customFetch(`clinician/resultsets?clinicianid=${clinicianid}&&startdate=${query.startdate}&&enddate=${query.enddate}`);
+    const { result, status, statusCode } = response;
+    if (statusCode == 200 && status == 'success' && result.length > 0) {
+      setData(result); setLoading(false);
+    } else {
+      setData([]); setLoading(false);
+    }
+  }, [query])
+
+  useEffect(() => {
+    getData(2);
+  }, []);
+
+
   return (
     <Divider>
-      <h1>Waiting List</h1>
+      <div className="filtering-waitinglist">
+        <select className="prime-select" onChange={(ev) => setQuery({ ...query, type: ev.target.value })}>
+          <option value="all">All</option>
+          <option value='sampling'>pending sampling</option>
+          <option value='processing'>sample processing</option>
+          <option value='ready' >result ready</option>
+          <option value='approved'>approved</option>
+        </select>
+        <input type="date" onChange={(ev) => setQuery({ ...query, startdate: ev.target.value })} className="prime-input mx-2" />
+        <input type="date" onChange={(ev) => setQuery({ ...query, enddate: ev.target.value })} className="prime-input mx-2" />
+        <button className="btn-orange" onClick={(ev) => { getData(2) }}>search</button>
+      </div>
+      <section className="my-2">
+        {loading && <h4 className="loader"></h4>}
+        {!loading && data.length > 0 && (
+          <>
+            <div className="list-group">
+              {data.map((item, index) => {
+                return (
+                  <div className="list-group-item front-filtering-client " key={index}>
+                    <p>{item?.billingid}</p>
+                    <p>{item?.fullname}</p>
+                    <p>{new Date(item?.billingdate).toLocaleDateString()}</p>
+                    <p>status: {item.ready_count}{"/" }{item.totaltest}</p>
+                  </div>
+                )
+              })}
+            </div>
+          </>
+        )}
+      </section>
     </Divider>
   )
 }
